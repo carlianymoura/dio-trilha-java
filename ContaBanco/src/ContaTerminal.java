e8e17c60e7a6ce0208bd5376bf024387d8f7036b
Carliany <100969517+carlianymoura@users.noreply.github.com>
2024-07-23 10:42:08 -0300

    Update ContaTerminal.java

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,9 @@
 public class ContaTerminal {
-    
+    public static void main (String [] args) {
+        int conta = 1021
+        string agencia = "067-8"    
+        string nome = "Mario Andrade"
+        double saldo = 237.48
 }
 /* Estruturas condicionais: if-else, switch-case 
 Estruturas de repetição: for, while, do-while
